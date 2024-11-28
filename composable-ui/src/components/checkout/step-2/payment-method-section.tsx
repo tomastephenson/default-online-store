d0f254dbe927ba7c44a01a34c98f4988a38b1b3c
Tom Stephenson <thomas.a.stephenson@gmail.com>
2024-11-28 13:30:10 +1300

    Update payment-method-section.tsx

--- composable-ui/src/components/checkout/step-2/payment-method-section.tsx
+++ composable-ui/src/components/checkout/step-2/payment-method-section.tsx
@@ -18,32 +18,10 @@ import { PAYMENT_METHOD } from '../constants'
 import { FiPlus } from 'react-icons/fi'
 import { memo } from 'react'
 
-const SetYourStripeAccount = () => (
-  <Stack bg={'info.200'} padding={4}>
-    <Text textStyle={'Desktop/Body-L'}>
-      To use stripe, add your own stripe keys to `.env`
-    </Text>
-    <Code>
-      <Text>STRIPE_SECRET_KEY=sk_test_...</Text>
-      <Text>NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY=pk_test_...</Text>
-    </Code>
-  </Stack>
-)
-
-interface PaymentMethodSectionProps {
-  stripeError?: boolean
-}
-
-export const PaymentMethodSection = memo(function PaymentMethodSection({
-  stripeError = false,
-}: PaymentMethodSectionProps) {
-  const intl = useIntl()
-  const {
-    paymentHandler: { register, select },
-  } = useCheckout()
-  const stripeAvailable =
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY && !stripeError
-
+  useEffect(() => {
+  // Set the initial isSelected state to true to expand the accordion
+  setIsSelected(true);
+    // Register only the stripe payment method
     register({
       key: PAYMENT_METHOD.STRIPE,
       title: intl.formatMessage({
@@ -53,7 +31,17 @@ export const PaymentMethodSection = memo(function PaymentMethodSection({
     })
   }, [intl, register])
 
-    <Box key={PAYMENT_METHOD.STRIPE} borderTop={0}>
+  const handleSelectPaymentMethod = (isSelected: boolean, key: string) => {
+    setIsSelected(true)  // Update isSelected state
+    if (isSelected) {
+      // Only allow selecting stripe
+      select(stripeAvailable ? key : undefined) // Select stripe if available, otherwise do nothing
+    } else {
+      select()
+    }
+  }
+
+  return (    <Box key={PAYMENT_METHOD.STRIPE} borderTop={0}>
             <Box flex="1" textAlign="left">
               {intl.formatMessage({
                 id: 'checkout.paymentSection.stripe.paymentMethodTitle',
@@ -71,7 +59,6 @@ export const PaymentMethodSection = memo(function PaymentMethodSection({
             </Box>
     </Box>
   )
-})
 
 const BillingAddressSubsection = () => {
   const intl = useIntl()
@@ -80,7 +67,6 @@ const BillingAddressSubsection = () => {
     config: { billingSameAsShipping },
   } = checkoutState
 
-  return (
     <Box mt={8}>
       <SectionHeader
         title={intl.formatMessage({
@@ -111,5 +97,4 @@ const BillingAddressSubsection = () => {
         }}
       />
     </Box>
-  )
 }
